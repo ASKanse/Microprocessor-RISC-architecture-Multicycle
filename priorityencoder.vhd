3702b4cec430e8616ea72d998e998caae9ca670f
ASKanse <32538213+ASKanse@users.noreply.github.com>
2018-10-25 23:57:15 +0530

    Update priorityencoder.vhd

--- priorityencoder.vhd
+++ priorityencoder.vhd
@@ -20,20 +20,28 @@ begin
 
 	if(pein(0) = '1') then
 		peout <= "111";
+	        pein(0) <= 0;
 	elsif(pein(1) = '1') then
 		peout <= "110";
+	        pein(1) <= 0;
 	elsif(pein(2) = '1') then
 		peout <= "101";
+	        pein(2) <= 0;
 	elsif(pein(3) = '1') then
-		peout <= "100";	
+		peout <= "100";
+	        pein(3) <= 0;
 	elsif(pein(4) = '1') then
 		peout <= "011";
+	        pein(4) <= 0;
 	elsif(pein(5) = '1') then
 		peout <= "010";
+                pein(5) <= 0;  
 	elsif(pein(6) = '1') then
 		peout <= "001";
+                pein(6) <= 0;
 	elsif(pein(7) = '1') then
 		peout <= "000";
+                pein(7) <= 0;
 	end if; 
 
 	if (pein = "00000000") then
