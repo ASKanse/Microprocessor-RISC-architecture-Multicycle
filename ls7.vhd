d30c0a83a7cbe1f45060534c933738900cb81a65
ASKanse <32538213+ASKanse@users.noreply.github.com>
2018-10-26 01:29:22 +0530

    Add files via upload

---
+++ ls7.vhd
@@ -0,0 +1,23 @@
+library std;
+use std.standard.all;
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity ls7 is 
+	port( ls7_in : in std_logic_vector(8 downto 0);
+		  ls7_out: out std_logic_vector(15 downto 0));
+end entity;
+
+architecture behave_ls7 of ls7 is 
+
+begin 
+ ls7_out(15 downto 7) <= ls7_in(8 downto 0); 
+ ls7_out(6) <= "0"; 
+ ls7_out(5) <= "0"; 
+ ls7_out(4) <= "0"; 
+ ls7_out(3) <= "0"; 
+ ls7_out(2) <= "0"; 
+ ls7_out(1) <= "0";
+ ls7_out(0) <= "0"; 
+end behave_se10; 
